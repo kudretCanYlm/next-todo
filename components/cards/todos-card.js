a0f44a0d931d476e4a8627b546ba7c74acbbd92a
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-08-28 17:16:14 +0300

    first commit

---
+++ components/cards/todos-card.js
@@ -0,0 +1,20 @@
+
+
+const TodosCard = ({ todos, className }) => {
+
+    return (
+        <div className={`todos-card ${className}`}>
+            <h2>ToDo's</h2>
+            <div className="todo-board">
+                <div className="top">
+
+                </div>
+                <div className="center">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default TodosCard;
