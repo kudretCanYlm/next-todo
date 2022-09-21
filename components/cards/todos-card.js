7e1243269915e984aaacbfc333570b782377c42c
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2022-09-21 20:42:29 +0300

    completed todo-cards

--- components/cards/todos-card.js
+++ components/cards/todos-card.js
@@ -1,16 +1,43 @@
 
 
-const TodosCard = ({ todos, className }) => {
+const TodosCard = ({ todos = [], className, deleteClick = id => id, updateClick = id => id }) => {
 
     return (
         <div className={`todos-card ${className}`}>
             <h2>ToDo's</h2>
             <div className="todo-board">
                 <div className="top">
-
+                    <h2>Todo</h2>
+                    <h2>Delete</h2>
+                    <h2>Update</h2>
                 </div>
+
                 <div className="center">
 
+                    {
+                        todos.length > 0 ?
+                            todos.map((todo, key) => (
+                                <div key={key} className="todo">
+                                    <div className="text">
+                                        <p>{todo.text}</p>
+                                    </div>
+                                    <div className="btn">
+                                        <button onClick={() => deleteClick(todo.id)} className="btn-del">
+                                            Delete
+                                        </button>
+                                        <button onClick={() => updateClick(todo.id)} className="btn-up">
+                                            Update
+                                        </button>
+                                    </div>
+
+                                </div>
+                            )) :
+                            (
+                                <h2>
+                                    There isn' any todo
+                                </h2>
+                            )
+                    }
                 </div>
             </div>
         </div>
